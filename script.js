2aadbada35dce4909cddfe65ed5c3eed6b287016
Michal Zybura <michalzybura96@gmail.com>
2022-09-26 18:36:53 +0200

    implementic the game logic

--- script.js
+++ script.js
@@ -1,12 +1,34 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 
+console.log(secretNumber);
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  console.log(guess, typeof guess);
+  console.log(guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = '❌ No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎆 Correct number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🧨 You lose. Try again';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '🧨 You lose. Try again';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
